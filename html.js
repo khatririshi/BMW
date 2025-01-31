dd4300b6ffb7d02c780c0a12dbd85057f6a96652
khatririshi <khatririshi37@gmail.com>
2025-02-01 00:31:06 +0530

    Add files via upload

---
+++ html.js
@@ -0,0 +1,297 @@
+// for(let i = 1; i <= 100; i++){
+//     console.log("I LOVE YOU JIYA");
+// }
+// let n = prompt("ENTER THE NUMBER :");
+
+// for(let i = 0;i <= n;i++){
+//     console.log("i =",i);
+// }
+
+// let i = 1;
+// while(i<=0){
+//     console.log("I LOVE YOU JIYA ");
+//     i++;
+// }
+
+
+// let age = 18;
+
+// if(age >= 18) // 18 is gretar then 16  and age >= 18 atle jo moti hoy ane sarkhi hoy to codition true thay
+// {   
+//     console.log("YOU CAN VOTE");
+// }
+// if(age < 18) 
+//     {
+//     console.log("you cannot vote");
+// }
+
+// let a = 18;
+
+// if(a > 35){
+//     console.log("TRUE");
+// }
+// else if(a < 15){
+//     console.log("IT IS ALSO TRUE");
+// }
+// else{
+//     console.log("IT IS ALSO FALSE");
+// }
+
+// for( let i = 1; i <= 5; i++){
+//     console.log("i = ", i);
+// }
+// for (let i = 6;i >= 1; i--){
+//     console.log(" i =", i);
+// }
+
+// let size = 0;
+// for (let i of str){
+//     console.log("i = ",i);
+//     size++;
+// }
+// console.log("Total size is ",size);
+// let str = "rishi";
+// let i;
+// for(i of str){
+//     console.log("i = ",i);
+// }
+
+// let student = {
+//     fullname :"rishi.s.khatri",
+//     love : "jiya",
+//     age : 19,
+//     cgpa : 5.15,
+// };
+// for (let i in student){
+//     console.log("", i, "=",student[i]);
+// }
+
+// for(let i= 0;i <= 100;i++){
+//       if(i % 2 ){
+//         console.log(" i =",i);
+//       }
+// }
+
+//GAME
+// let game = 45;
+// let user = prompt("guess the number");
+
+// while(user != game){
+//     user = prompt("this is wrong input");
+// }
+// console.log("yoy will win");
+
+// let fullname = `this is tempet litres`;
+// console.log(fullname); 
+
+// let obj={
+//    item : "PEN",
+//    price : 1200,
+// };
+// let output = `THE COST OF ${obj.item} IS ${obj.price}`;
+// console.log(output);
+
+
+// let str = "                      rishikhatrijs            " ;
+// console.log(str.trim());
+
+// let fullname = prompt("ENTER YOU FULL NAME WITHOUT SPACE: ");
+// let username = "@" + fullname + fullname.length;
+// console.log(username);
+let one =["Rishi","raj","hella","jay"];
+
+for(let ind = 0;ind < one.length;ind++){
+    console.log(one[ind]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let score = prompt("ENTER THE SCORE OF STUDENT");
+// let grade;
+
+// if(score >= 90 && score <=100){
+//     grade = "A";
+// }
+// else if(score >= 80 && score <= 89){
+//     grade = "B";
+// }
+// else if(score >= 70 && score <=79){
+//     grade = "C"
+// }
+// else if(score >= 60 && score <=69){
+//     grade = "D"
+// }
+// else if(score >= 50 && score <=59){
+//     grade = "E"
+// }
+// console.log(grade);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let age = 16;
+
+// let ans = age > 18 ? "YOU CAN VOTE" : "YOU CAN'T VOTE ";
+
+// console.log(ans);
+
+
+
+// let mode = "pink ";
+// let color;
+
+// if(mode == "dark"){
+//     color = "black";
+// }
+// else if(mode == "blue"){
+//     color = "blue";
+// }
+// else if(mode == "pink "){
+//     color = "pink";
+// }
+// else if(mode == "yello"){
+//     color = "yello";
+// }
+// else{
+//     color = "white";
+// }
+// console.log(color);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let num = 20;
+
+// if( num % 2 === 0) {
+//     console.log("even");
+// }
+// else{
+//     console.log("odd");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let mode = "yello";
+// let color;
+
+// if(mode === "dark"){
+//     color = "black";
+// }
+// else{
+//     color = "white";
+// }
+
+// console.log(color);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let age = 18;
+// let mode = "dark";
+// let color;
+
+
+// if(age >= 18){
+//     console.log("NOW YOU CAN VOT");
+// }
+
+// if(age < 18){
+//     console.log("YOU CAN'T VOT YET");
+// }
